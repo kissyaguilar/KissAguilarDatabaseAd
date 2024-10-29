85d8fc9daa0abfd9d96384f821c83a61dd385d6e
Kissy Aguilar <kissyaguilar1@gmail.com>
2024-10-29 12:40:58 +0800

    update dbName

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
   $dbhost = "localhost";
 	$dbuser = "root";
 	$dbpass = "";
-	$db = "demo";
+	$db = "connecthub";
 
 	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
