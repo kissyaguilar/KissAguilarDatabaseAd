587921178181e5ca4138e7917662794c1672d01d
Kissy Aguilar <kissyaguilar1@gmail.com>
2024-10-30 17:47:12 +0800

    My Message App using SELECT QUERY for all texts, and pictures

--- A05/index.php
+++ A05/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -25,13 +25,15 @@
                         
                         // Fetch Group Chat Picture
                         $groupChatID = 1;
-                        $queryGroupChat = "SELECT picture, theme, voiceCall, videoChat, more, attachment, gallery, GIF FROM groupChats WHERE groupChatID = $groupChatID"; 
+                        $queryGroupChat = "SELECT picture, theme, voiceCall, videoChat, more, attachment, gallery, GIF, sendMessage 
+                                           FROM groupChats 
+                                           WHERE groupChatID = $groupChatID"; 
                         $resultGroupChat = executeQuery($queryGroupChat);
 
                         if ($resultGroupChat && mysqli_num_rows($resultGroupChat) > 0) {
                             $groupChatData = mysqli_fetch_assoc($resultGroupChat);
                             $groupChatPicture = $groupChatData['picture'];
-                            $theme = $groupChatData['theme']; 
+                            $theme = $groupChatData['theme'];
                             echo "<img src='images/" . htmlspecialchars($groupChatPicture) . "' alt='Group Chat Picture'>";
                         } else {
                             echo "<img src='images/default-group.png' alt='Default Group Chat Picture'>";
@@ -39,14 +41,15 @@
                     ?>
                 </div>
                 <div class="group-chat-name">Emotions</div>
-
             </div>
+            
             <div class="header-icons">
-            <?php
-            echo "<div class='icon call' style='background-image: url(images/" . htmlspecialchars($groupChatData['voiceCall']) . ");'></div>";
-            echo "<div class='icon videoChat' style='background-image: url(images/" . htmlspecialchars($groupChatData['videoChat']) . ");'></div>";
-            echo "<div class='icon more' style='background-image: url(images/" . htmlspecialchars($groupChatData['more']) . ");'></div>";
-            ?>
+                <!-- Displaying icons for call, vc, more -->
+                <?php
+                    echo "<div class='icon call' style='background-image: url(images/" . htmlspecialchars($groupChatData['voiceCall']) . ");'></div>";
+                    echo "<div class='icon videoChat' style='background-image: url(images/" . htmlspecialchars($groupChatData['videoChat']) . ");'></div>";
+                    echo "<div class='icon more' style='background-image: url(images/" . htmlspecialchars($groupChatData['more']) . ");'></div>";
+                ?>
             </div>
         </div>
 
@@ -93,15 +96,17 @@
         <!-- Input Section (Typing display - does not insert) -->
         <div class="input-section">
             <div class="input-icons">
-            <?php
-            echo "<div class='icon attachment' style='background-image: url(images/" . htmlspecialchars($groupChatData['attachment']) . ");'></div>";
-            echo "<div class='icon gallery' style='background-image: url(images/" . htmlspecialchars($groupChatData['gallery']) . ");'></div>";
-            echo "<div class='icon GIF' style='background-image: url(images/" . htmlspecialchars($groupChatData['GIF']) . ");'></div>";
-            ?>
-              
+                <!-- Displaying icons beside the typing area -->
+                <?php
+                    echo "<div class='icon attachment' style='background-image: url(images/" . htmlspecialchars($groupChatData['attachment']) . ");'></div>";
+                    echo "<div class='icon gallery' style='background-image: url(images/" . htmlspecialchars($groupChatData['gallery']) . ");'></div>";
+                    echo "<div class='icon GIF' style='background-image: url(images/" . htmlspecialchars($groupChatData['GIF']) . ");'></div>";
+                ?>
             </div>
             <input type="text" class="message-input" placeholder="Aa">
-            <div class="icon send"></div>
+            <?php
+                echo "<div class='icon send' style='background-image: url(images/" . htmlspecialchars($groupChatData['sendMessage']) . ");'></div>";
+            ?>
         </div>
     </div>
 </body>
