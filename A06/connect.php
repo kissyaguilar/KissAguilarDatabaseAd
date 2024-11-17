d7d92fadb55da618f6fd80dc3a0ea2af69294ca9
kissyaguilar <kissyaguilar1@gmail.com>
2024-11-18 03:38:45 +0800

    Update connect.php

--- A06/connect.php
+++ A06/connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root"; // Default for XAMPP
 $password = ""; // Default for XAMPP
-$dbname = "connecthub";
+$dbname = "connectHub";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
