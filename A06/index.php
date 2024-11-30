da9ca97da94b98d84243ed9ce29053d27a614b1a
kissyaguilar <kissyaguilar1@gmail.com>
2024-11-30 11:27:06 +0800

    Update index.php

--- A06/index.php
+++ A06/index.php
@@ -21,7 +21,8 @@ if (isset($_GET['error']) && $_GET['error'] == 'emptyMessage') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="images/logoTab.png" type="image/png">
     <title>EmotiChat Group Chat</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
     <style>
         body {
@@ -382,6 +383,11 @@ if (isset($_GET['error']) && $_GET['error'] == 'emptyMessage') {
                     <?php echo $errorMessage; ?>
                 </div>
             <?php endif; ?>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+                crossorigin="anonymous">
+                </script>
 </body>
 
 </html>
